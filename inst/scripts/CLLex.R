016686f5c2aee19148e5d5b80371c1501bbe0d1c
Robert Gentleman <rgentlem@fhcrc.org>
2006-01-25 21:58:19 +0000

    tidied up the script a little
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Category@15881 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/scripts/CLLex.R
+++ inst/scripts/CLLex.R
@@ -31,15 +31,9 @@ cache <- function(name, expr) {
 
  rwtt = rowttests(CLL.sub, CLL.sub$Disease)
 
- library(Category)
+ library("Category")
 
  fL = findLargest(geneNames(CLL.sub), abs(rwtt$statistic), "hgu95av2")
-
- probes2MAP <- function (pids, data = "hgu133plus2") {
-    pEnv = get(paste(data, "MAP", sep = ""))
-    inMAP = mget(pids, pEnv, ifnotfound = NA)
-    inMAP[!is.na(inMAP)]
-}
 
 fL2 = probes2MAP(fL, "hgu95av2")
 length(fL2)
@@ -51,30 +45,6 @@ fL = fL[inBoth]
 
  tobs = rowttests(eS, "Disease")
 
- MAPAmat <- function (data, minCount=5) {
-    if (!is.character(data) || length(data) != 1)
-        stop("wrong argument") 
-    dataE = as.list(get(paste(data, "MAP", sep = "")))
-    LLs = getLL(names(dataE), data) 
-    goodLLs = !duplicated(LLs) & !is.na(LLs)
-    use = dataE[goodLLs]
-    names(use) = LLs[goodLLs]
-    ##some are in more than one cytochrome band and we keep only the first
-    use2 = sapply(use, function(x) x[1])
-    cytoLL = split(names(use2), use2)
-    cts = sapply(cytoLL, length)
-    goodCts = cts >= minCount
-    cytoLL = cytoLL[goodCts]
-    uniqLL = unique(unlist(cytoLL))
-    Amat = sapply(cytoLL, function(x) {
-        mtch = match(x, uniqLL)
-        zeros = rep(0, length(uniqLL))
-        zeros[mtch] = 1
-        zeros
-    })
-    dimnames(Amat) = list(uniqLL, names(cytoLL))
-    return(Amat)
-}
 
  z1 = MAPAmat("hgu95av2")
 
