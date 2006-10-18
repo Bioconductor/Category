0bd9ab62a1fac503ba4b0be8fe240a7aba2077f4
Seth Falcon <sfalcon@fhcrc.org>
2006-10-18 14:55:30 +0000

    Update regression unit test to new annotation data.
    Also added a test for the YEAST chip.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Category@20643 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/UnitTests/hyperGTest_test.R
+++ inst/UnitTests/hyperGTest_test.R
@@ -1,22 +1,27 @@
 library("hgu95av2")
+library("YEAST")
 
-makeSimpleGOHyperGParams <- function() {
+makeSimpleGOHyperGParams <- function(chip=c("hgu95av2", "YEAST")) {
     set.seed(344)
-    probeIds <- ls(hgu95av2LOCUSID)
+    chip <- match.arg(chip)
+    if (chip == "hgu95av2") {
+        probeIds <- ls(hgu95av2ENTREZID)
+    } else if (chip == "YEAST") {
+        probeIds <- ls(YEASTCHR)
+    }
     randProbeIds <- sample(probeIds, 500)
-##     entrezUniverse <- unlist(mget(randProbeIds, hgu95av2LOCUSID,
-##                                   ifnotfound=NA))
-    ## This is "wrong", should unlist, but the code
-    ## should catch/correct it.  The right way is above.
-    entrezUniverse <- mget(randProbeIds, hgu95av2LOCUSID,
-                           ifnotfound=NA)
-
+    if (chip == "YEAST") {
+        entrezUniverse <- randProbeIds
+    } else {
+        entrezUniverse <- mget(randProbeIds, hgu95av2ENTREZID,
+                               ifnotfound=NA)
+    }
     entrezUniverse <- entrezUniverse[!is.na(entrezUniverse)]
     selectedEntrezIds <- sample(entrezUniverse, 30)
     params <- new("GOHyperGParams",
                   geneIds=selectedEntrezIds, 
                   universeGeneIds=entrezUniverse,
-                  annotation="hgu95av2", 
+                  annotation=chip, 
                   ontology="BP",
                   pvalueCutoff=0.05,
                   conditional=FALSE,
@@ -25,25 +30,49 @@ makeSimpleGOHyperGParams <- function() {
 }
     
 
-test_basic_regression <- function() {
-    p <- makeSimpleGOHyperGParams()
+test_basic_regression_hgu95av2 <- function() {
+    p <- makeSimpleGOHyperGParams(chip="hgu95av2")
     res <- hyperGTest(p)
-    checkEquals(18, sum(pvalues(res) < pvalueCutoff(res)))
+    checkEquals(22, sum(pvalues(res) < pvalueCutoff(res)))
 
-    pvals <- round(c(0.01596240, 0.01825930, 0.02194761), 3)
-    names(pvals) <- c("GO:0043170", "GO:0044265", "GO:0009057") 
-    checkEquals(pvals, round(pvalues(res)[1:3], 3))
+    pvals <- round(c(0.001, 0.001, 0.002), 3)
+    names(pvals) <- c("GO:0051247", "GO:0009891", "GO:0044267") 
+    checkEquals(pvals, round(pvalues(res)[3:5], 3))
     
-    gcounts <- c(13, 4, 4)
-    names(gcounts) <- c("GO:0043170", "GO:0044265", "GO:0009057")
-    checkEquals(gcounts, geneCounts(res)[1:3])
+    gcounts <- c(3, 3, 13)
+    names(gcounts) <- names(pvals)
+    checkEquals(gcounts, geneCounts(res)[3:5])
 
-    ucounts <- c(134, 19, 20)
-    names(ucounts) <- c("GO:0043170", "GO:0044265", "GO:0009057")
-    checkEquals(ucounts, universeCounts(res)[1:3])
+    ucounts <- c(4, 4, 95)
+    names(ucounts) <- names(pvals)
+    checkEquals(ucounts, universeCounts(res)[3:5])
     
-    checkEquals(381, universeMappedCount(res))
-    checkEquals(22, geneMappedCount(res))
+    checkEquals(390, universeMappedCount(res))
+    checkEquals(25, geneMappedCount(res))
     checkEquals("hgu95av2", annotation(res))
+    checkEquals(c("GO", "BP"), testName(res))
+}
+
+
+test_basic_regression_YEAST <- function() {
+    p <- makeSimpleGOHyperGParams(chip="YEAST")
+    res <- hyperGTest(p)
+    checkEquals(2, sum(pvalues(res) < pvalueCutoff(res)))
+
+    pvals <- round(c(0.06, 0.06, 0.06), 3)
+    names(pvals) <- c("GO:0045332", "GO:0007009", "GO:0045490")
+    checkEquals(pvals, round(pvalues(res)[3:5], 3))
+    
+    gcounts <- c(1, 1, 1)
+    names(gcounts) <- names(pvals)
+    checkEquals(gcounts, geneCounts(res)[3:5])
+
+    ucounts <- c(1, 1, 1)
+    names(ucounts) <- names(pvals)
+    checkEquals(ucounts, universeCounts(res)[3:5])
+    
+    checkEquals(500, universeMappedCount(res))
+    checkEquals(30, geneMappedCount(res))
+    checkEquals("YEAST", annotation(res))
     checkEquals(c("GO", "BP"), testName(res))
 }
