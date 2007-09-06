fba5c3ff7137e50d61d286275eb357734ce2ae9a
Seth Falcon <sfalcon@fhcrc.org>
2007-09-06 17:56:59 +0000

    Rename chrBandInciMat => MAPAmat
    chrBandInciMat is still available, but deprecated.  The behavior of
    MAPAmat has changed (see man page).  Any users of MAPAmat will need to
    revisit their code.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Category@26896 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/MAPcode.R
+++ R/MAPcode.R
@@ -11,31 +11,6 @@ probes2MAP <- function (pids, data = "hgu133plus2") {
     inMAP[!is.na(inMAP)]
 }
 
-MAPAmat <- function (data, minCount=5) {
-    if (!is.character(data) || length(data) != 1)
-        stop("wrong argument")
-    dataE = as.list(getAnnMap("MAP", chip=data))
-    LLs = getLL(names(dataE), data)
-    goodLLs = !duplicated(LLs) & !is.na(LLs)
-    use = dataE[goodLLs]
-    names(use) = LLs[goodLLs]
-    ##some are in more than one cytochrome band and we keep only the first
-    use2 = sapply(use, function(x) x[1])
-    cytoLL = split(names(use2), use2)
-    cts = sapply(cytoLL, length)
-    goodCts = cts >= minCount
-    cytoLL = cytoLL[goodCts]
-    uniqLL = unique(unlist(cytoLL))
-    Amat = sapply(cytoLL, function(x) {
-        mtch = match(x, uniqLL)
-        zeros = rep(0, length(uniqLL))
-        zeros[mtch] = 1
-        zeros
-    })
-    dimnames(Amat) = list(uniqLL, names(cytoLL))
-    return(Amat)
-}
-
 
 ### ------------------------------------------------------------------
 
@@ -336,8 +311,18 @@ makeChrBandInciMat <- function(chrGraph) {
     mat
 }
 
-chrBandInciMat <- function(chip, univ=NULL) {
-    makeChrBandInciMat(makeChrBandGraph(chip=chip, univ=univ))
+MAPAmat <- function(chip, univ=NULL, minCount=0) {
+    m <- makeChrBandInciMat(makeChrBandGraph(chip=chip, univ=univ))
+    if (minCount > 0) {
+        rs <- rowSums(m)
+        m <- m[rs >= minCount, ]
+    }
+    m
+}
+
+chrBandInciMat <- function(chip, univ=NULL, minCount=0) {
+    .Deprecated("MAPAmat")
+    MAPAmat(chip=chip, univ=univ, minCount=minCount)
 }
 
 ann_list_to_mat <- function(L) {
