e7051ce80d1cec8a631f6c20db00a18edffecbbb
Marc Carlson <mcarlson@fhcrc.org>
2012-11-27 19:14:04 +0000

    Updates syncs client code with changes to the server.
    Server changed the way serveral things were represented.  These
    changes make necessary adjustments and also make it simpler in the
    future to adjust to changes in the server host.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Category@71491 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utils.R
+++ R/utils.R
@@ -2,6 +2,8 @@ getDataEnv <- function(name, lib) {
     get(paste(lib, name, sep=""))
 }
 
+## here getAnnMap is NOT returning a bimap.
+## It's also not really accessing one.
 getOrganism <- function(chip){
     paste("ORGANISM:", getAnnMap("ORGANISM", chip), sep="")
 }
